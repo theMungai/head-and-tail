8bee722741d75e6a5518c9ac3febca17deeeea65
theMungai <mungaithesimon@gmail.com>
2024-11-11 12:59:45 +0300

    Head and Tail

--- script.js
+++ script.js
@@ -2,16 +2,42 @@ const headButton = document.querySelector("#head");
 const tailButton = document.querySelector("#tail");
 let choice = ["head", "tail"]
 
+let score = {
+    wins : 0,
+    losses : 0
+}
+
+let results 
+
+// Updating Scores 
+if(results === "You win"){
+    score.wins = score.wins + 1
+}
+else if(results === "You Lose"){
+    score.losses = score.losses + 1
+}
+
 headButton.addEventListener('click', () => {
     const computerChoice = Math.floor(Math.random() * choice.length);
     let finalChoice = choice[computerChoice];
 
     if(finalChoice === "tail"){
-        console.log("You win")
+        results = "You win"
+        console.log(results)
     }
     else if(finalChoice  === "head"){
-        console.log("You Lose")
+        results = "You Lose"
+        console.log(results)
     }
+
+        // Updating Scores 
+    if(results === "You win"){
+        score.wins = score.wins + 1
+    }
+    else if(results === "You Lose"){
+        score.losses = score.losses + 1
+    }
+    console.log(score)
 })
 
 tailButton.addEventListener('click', () => {
@@ -19,10 +45,22 @@ tailButton.addEventListener('click', () => {
     let finalChoice = choice[computerChoice];
 
     if(finalChoice === "tail"){
-        console.log("You Lose")
+        results = "You Lose"
+        console.log(results)
     }
 
     else if(finalChoice === "head"){
-        console.log("You win")
+        results = "You win"
+        console.log(results)
     }
+
+        // Updating Scores 
+    if(results === "You win"){
+        score.wins = score.wins + 1
+    }
+    else if(results === "You Lose"){
+        score.losses = score.losses + 1
+    }
+    console.log(score)
 })
+
